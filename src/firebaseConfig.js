733160b27617d338a544da538a9ae6a288ec6636
bcrescitelli <brandoncrescitelli@yahoo.com>
2026-01-15 18:23:18 -0500

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -9,12 +9,10 @@ const firebaseConfig = {
   storageBucket: "branbox-hub.firebasestorage.app",
   messagingSenderId: "377780537570",
   appId: "1:377780537570:web:0b03afb2b32dacf4f836bb",
-  databaseURL: "https://branbox-hub-default-rtdb.firebaseio.com" // Standard URL for Firebase RTDB
+  databaseURL: "https://branbox-hub-default-rtdb.firebaseio.com"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export the "Tools" so the games can use them
 export const db = getDatabase(app);
 export const auth = getAuth(app);
